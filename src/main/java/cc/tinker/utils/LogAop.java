d31197e48560ece851a5f6a0926201580f67a6f4
whoszus <whoszus@yahoo.com>
2016-12-21 00:02:59 +0800

    1. Authentication  Component commit ; 2. spring boot  CORS init;

---
+++ src/main/java/cc/tinker/utils/LogAop.java
@@ -0,0 +1,64 @@
+package cc.tinker.utils;
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * Created by Tinker on 2016/12/19.
+ */
+
+@Aspect
+@Component
+public class LogAop {
+
+//    @Value("${spring.profiles}")
+//    private String env;
+
+    private Logger logger = Logger.getLogger(getClass());
+    @Pointcut("execution( public * cc.tinker.controller.*.*(..))")
+//    @Pointcut("execution(public * cc.tinker.controller..*(..)) and @annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    public void webLog(){
+    }
+
+
+    /**
+     * 切点前；
+     * @param joinPoint
+     * @throws Throwable
+     */
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+    /**
+     *  返回后
+     * @param ret
+     * @throws Throwable
+     */
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        logger.info("RESPONSE : " + ret);
+    }
+
+
+}
+
