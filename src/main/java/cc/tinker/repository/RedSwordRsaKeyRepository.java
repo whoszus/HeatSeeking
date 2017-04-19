71085d0e1652c961f5f8141ec2144e6122ac216d
whoszus <whoszus@yahoo.com>
2017-04-19 15:26:13 +0800

    handover - 2017.4.11

---
+++ src/main/java/cc/tinker/repository/RedSwordRsaKeyRepository.java
@@ -0,0 +1,18 @@
+package cc.tinker.repository;
+
+import cc.tinker.entity.RedswordRsaKeyEntity;
+import org.springframework.data.jpa.repository.Query;
+import tinker.entr.repository.BaseRepository;
+
+/**
+ * Created by whoszus on 2017/4/11.
+ *
+ * @email whoszus@yahoo.com
+ */
+public interface RedSwordRsaKeyRepository extends BaseRepository<RedswordRsaKeyEntity,Integer> {
+
+    @Query("from RedswordRsaKeyEntity r where r.userId = ?1")
+    RedswordRsaKeyEntity findOneByUserId(Integer userId);
+
+
+}
