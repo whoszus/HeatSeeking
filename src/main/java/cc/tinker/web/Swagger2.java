f01a46b9e47e2f3d993d245a9efa7c0555187bee
tinker <whoszus@yahoo.com>
2017-06-08 17:26:43 +0800

    添加Swagger2  管理使用API文档；

--- src/main/java/cc/tinker/web/Swagger2.java
+++ src/main/java/cc/tinker/web/Swagger2.java
@@ -28,9 +28,9 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
-                .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("程序猿DD")
+                .description("")
+                .termsOfServiceUrl("")
+                .contact("tinker")
                 .version("1.0")
                 .build();
     }
